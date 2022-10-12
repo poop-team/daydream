9264d09a2c3ded2bc5c7c5480d1a89b907d37504
Rikimbili <raciel2901@gmail.com>
2022-10-12 15:37:32 -0400

    refactor(*): Run ESLint --fix

--- web/vitest.config.ts
+++ web/vitest.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vitest/config'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vitest/config';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,4 +7,4 @@ export default defineConfig({
   test: {
     environment: 'jsdom',
   },
-})
+});
