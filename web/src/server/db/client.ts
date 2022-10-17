2c8cd5d5aa5c078548af28b4a67540c221ab4de9
Sleepingpirates <faizsyed.a@gmail.com>
2022-10-17 14:54:17 -0400

    build(Schemas): added schemas for env variables.

---
+++ web/src/server/db/client.ts
@@ -0,0 +1,20 @@
+// src/server/db/client.ts
+import { PrismaClient } from "@prisma/client";
+
+import { env } from "../../env/server.mjs";
+
+declare global {
+  // eslint-disable-next-line no-var, no-unused-vars
+  var prisma: PrismaClient | undefined;
+}
+
+export const prisma =
+  global.prisma ||
+  new PrismaClient({
+    log:
+      env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
+  });
+
+if (env.NODE_ENV !== "production") {
+  global.prisma = prisma;
+}
