985e3ba0bb72a6c4f399ff93cc3ba8c4f3603e63
Sleepingpirates <faizsyed.a@gmail.com>
2022-10-17 14:54:10 -0400

    feat(nextauth): added google and github oauth.

--- web/src/pages/api/auth/[...nextauth].ts
+++ web/src/pages/api/auth/[...nextauth].ts
@@ -1,44 +1,41 @@
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import { PrismaClient } from "@prisma/client";
 import NextAuth, { NextAuthOptions } from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
 
-const prisma = new PrismaClient();
+import { env } from "../../../env/server.mjs";
+import { prisma } from "../../../server/db/client";
+
 
 /*
  *  NextAuth configuration
  *  All requests to /api/auth/* (signIn, callback, signOut, etc.) will automatically be handled by NextAuth using this config
  */
 export const authOptions: NextAuthOptions = {
+  callbacks: {
+    session({ session, user }) {
+      if (session.user) {
+        session.user.id = user.id;
+      }
+      return session;
+    },
+  },
+
+
   adapter: PrismaAdapter(prisma),
   // Configure one or more authentication providers
   providers: [
     GithubProvider({
-      clientId: process.env.GITHUB_ID as string,
-      clientSecret: process.env.GITHUB_SECRET as string,
+      clientId: env.GITHUB_ID,
+      clientSecret: env.GITHUB_SECRET,
     }),
     GoogleProvider({
-      clientId: process.env.GOOGLE_ID as string,
-      clientSecret: process.env.GOOGLE_SECRET as string,
+      clientId: env.GOOGLE_ID,
+      clientSecret: env.GOOGLE_SECRET,
     }),
   ],
-  // callbacks: {
-  //   // Ensure that the user id and email is passed to the client's cookie
-  //   async jwt({ token, user }) {
-  //     token.email = user?.email;
-  //     return token;
-  //   },
-  //   async session({ session, token }) {
-  //     if (token) {
-  //       if (session.user) {
-  //         session.user.email = token.email;
-  //       }
-  //     }
-  //     return session;
-  //   },
-  // },
-  secret: process.env.NEXTAUTH_SECRET,
+
+  secret: env.NEXTAUTH_SECRET,
   // TODO: Enable pages after testing
   // pages: {
   //   signIn: "/auth?action=login",
@@ -46,7 +43,7 @@ export const authOptions: NextAuthOptions = {
   //   signOut: "/auth",
   //   error: "/auth",
   // },
-  debug: process.env.NODE_ENV === "development",
+  //debug: process.env.NODE_ENV === "development",
 };
 
 export default NextAuth(authOptions);
