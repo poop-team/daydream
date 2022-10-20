2863bfb2bb9882ccbfbcef43a93ff269fb4680cb
Rikimbili <raciel2901@gmail.com>
2022-10-19 20:18:45 -0400

    fix(vitest.config.ts): Type error workaround

--- web/vitest.config.ts
+++ web/vitest.config.ts
@@ -1,10 +1,11 @@
-import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vitest/config';
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vitest/config";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  // TODO: Remove this cast once the stable upstream version fixes the type. Currently borked on 3.1.8
+  plugins: [react() as any],
   test: {
-    environment: 'jsdom',
+    environment: "jsdom",
   },
 });
