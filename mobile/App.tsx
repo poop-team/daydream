59637972cdf3e61cfc0f35b8f5a47065e573a6d1
kim -aka victor <31045060+kitorer@users.noreply.github.com>
2022-10-20 23:13:50 -0400

    UI exsists
    todo:
    transparency-blur aka backdrop-filter
    none of the js exists yet

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,12 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
-
+import { StyleSheet, Text, View } from 'react-native';
+import  BottomNavbar  from './src/components/navbar';
 export default function App() {
   const value: string = "hello, world";
   return (
     <View className="flex-1 justify-center items-center">
       <Text>{value}</Text>
-      <StatusBar style="auto" />
+      <BottomNavbar />
     </View>
   );
 }
