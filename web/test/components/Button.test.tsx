fb7c9589338efb5b9fc1d5e2a8512d9820a2d3ba
Rikimbili <raciel2901@gmail.com>
2022-10-20 23:41:48 -0400

    refactor(Button.test): Move to test directory

--- web/test/components/Button.test.tsx
+++ web/test/components/Button.test.tsx
@@ -1,7 +1,7 @@
 import { fireEvent, render, screen } from "@testing-library/react";
 import { describe, expect, it, vi } from "vitest";
 
-import Button from "./Button";
+import Button from "../../src/components/Inputs/Button";
 
 describe("Button", () => {
   it("should render", () => {
