f92645d618f769f728adf97957921ddcc1dfa212
kitorer <cockatielcodeh2@gmail.com>
2022-10-21 22:02:02 -0400

    apptsx shouldnt contain anything other than pages

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,12 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import  BottomNavbar  from './src/components/navbar';
+import FeedPage from './src/feedPage';
 export default function App() {
   const value: string = "hello, world";
   return (
-    <View className="flex-1 justify-center items-center">
-      <Text>{value}</Text>
-      <BottomNavbar />
-    </View>
+
+      <FeedPage />
+    
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+
+  },
+});
