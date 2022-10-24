16eed2255cdfe2cba8892c4bbf1ffe4c671a52c6
kitorer <cockateilcodeh2@gmail.com>
2022-10-24 18:30:02 -0400

    cleanup & color change

--- mobile/src/components/Button.tsx
+++ mobile/src/components/Button.tsx
@@ -1,13 +1,11 @@
 import { Pressable, Text, View } from 'react-native';
 
-/*
-
-*/
-//pass in value as a prop
-export default function ButtonA(props) {
+function ButtonPill(props) {
   return (
-    <Pressable className= "rounded-full bg-indigo-500 h-7 items-center justify-center" >
+    <Pressable className= "rounded-full bg-[#2D1C70] opacity-90 h-7 items-center justify-center" >
         <Text className='text-white margin mx-5'>{props.name}</Text>
     </Pressable>
   );
 }
+
+export { ButtonPill };
