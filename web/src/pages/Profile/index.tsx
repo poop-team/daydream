7d536122191d8dd35084e752c1ce55ea86a82b19
dark-r00t <79925808+dark-r00t@users.noreply.github.com>
2022-10-24 19:40:31 -0400

    feat: added more basic page elements

--- web/src/pages/Profile/index.tsx
+++ web/src/pages/Profile/index.tsx
@@ -1,3 +1,4 @@
+import Button from "../../components/Inputs/Button";
 import Card from "../../components/Surfaces/Card";
 
 /*
@@ -33,61 +34,104 @@ export default function Index() {
       </div>
 
       {/* USERNAME */}
-      {/* VIEWS / SAVED */}
-      {/* CREATED / COLLECTIONS */}
-      {/* SEARCH / +CREATE */}
+      <div className="container mx-auto">
+        <div className="flex flex-wrap items-center justify-center">
+          <div className="text-lg">@username</div>
+        </div>
+        <div className="p-1"></div>
+      </div>
 
-      {/* CARDS */}
-      <div className="px-4">
-        <div className="flex flex-wrap items-center justify-center gap-8">
-          <section className={"flex gap-4"}>
-            <Card className={"h-80 w-80 bg-slate-600"} />
-          </section>
-          <section className={"flex gap-4"}>
-            <Card className={"h-80 w-80 bg-slate-700"} />
-          </section>
-          <section className={"flex gap-4"}>
-            <Card className={"h-80 w-80 bg-slate-600"} />
-          </section>
-          <section className={"flex gap-4"}>
-            <Card className={"h-80 w-80 bg-slate-700"} />
-          </section>
-          <section className={"flex gap-4"}>
-            <Card className={"h-80 w-80 bg-slate-600"} />
-          </section>
-          <section className={"flex gap-4"}>
-            <Card className={"h-80 w-80 bg-slate-700"} />
-          </section>
-          <section className={"flex gap-4"}>
-            <Card className={"h-80 w-80 bg-slate-600"} />
-          </section>
-          <section className={"flex gap-4"}>
-            <Card className={"h-80 w-80 bg-slate-700"} />
-          </section>
-          <section className={"flex gap-4"}>
-            <Card className={"h-80 w-80 bg-slate-600"} />
-          </section>
-          <section className={"flex gap-4"}>
-            <Card className={"h-80 w-80 bg-slate-700"} />
-          </section>
-          <section className={"flex gap-4"}>
-            <Card className={"h-80 w-80 bg-slate-600"} />
-          </section>
-          <section className={"flex gap-4"}>
-            <Card className={"h-80 w-80 bg-slate-700"} />
-          </section>
-          <section className={"flex gap-4"}>
-            <Card className={"h-80 w-80 bg-slate-600"} />
-          </section>
-          <section className={"flex gap-4"}>
-            <Card className={"h-80 w-80 bg-slate-700"} />
-          </section>
-          <section className={"flex gap-4"}>
-            <Card className={"h-80 w-80 bg-slate-600"} />
-          </section>
-          <section className={"flex gap-4"}>
-            <Card className={"h-80 w-80 bg-slate-700"} />
-          </section>
+      {/* VIEWS / SAVED */}
+      <div className="container mx-auto">
+        <div className="flex flex-wrap items-center justify-center gap-2">
+          <div className="text-sm">0 views</div>
+          <div className="text-sm">0 saves</div>
+        </div>
+        <div className="p-2"></div>
+      </div>
+
+      {/* CREATED / COLLECTIONS */}
+      <div className="container mx-auto">
+        <div className="p-2"></div>
+        <div className="flex flex-wrap items-center justify-center gap-2">
+          <div className="p-2">
+            <section className={"flex gap-4"}>
+              <div className={"flex flex-col items-center gap-2"}>
+                <Button disabled>Created</Button>
+              </div>
+              <div className={"flex flex-col items-center gap-2"}>
+                <Button>Collections</Button>
+              </div>
+            </section>
+          </div>
+        </div>
+        <div className="p-2"></div>
+      </div>
+
+      {/* SEARCH / +CREATE */}
+      <div className="container mx-auto">
+        <div className="p-2"></div>
+        <div className="flex flex-wrap items-center justify-center gap-2">
+          <div className="p-2"></div>
+        </div>
+        <div className="p-2"></div>
+      </div>
+
+      {/* CARDS
+        may want to remove container... idk yet
+      */}
+      <div className="container mx-auto">
+        <div className="px-4">
+          <div className="flex flex-wrap items-center justify-center gap-8">
+            <section className={"flex gap-4"}>
+              <Card className={"h-80 w-80 bg-slate-600"} />
+            </section>
+            <section className={"flex gap-4"}>
+              <Card className={"h-80 w-80 bg-slate-700"} />
+            </section>
+            <section className={"flex gap-4"}>
+              <Card className={"h-80 w-80 bg-slate-600"} />
+            </section>
+            <section className={"flex gap-4"}>
+              <Card className={"h-80 w-80 bg-slate-700"} />
+            </section>
+            <section className={"flex gap-4"}>
+              <Card className={"h-80 w-80 bg-slate-600"} />
+            </section>
+            <section className={"flex gap-4"}>
+              <Card className={"h-80 w-80 bg-slate-700"} />
+            </section>
+            <section className={"flex gap-4"}>
+              <Card className={"h-80 w-80 bg-slate-600"} />
+            </section>
+            <section className={"flex gap-4"}>
+              <Card className={"h-80 w-80 bg-slate-700"} />
+            </section>
+            <section className={"flex gap-4"}>
+              <Card className={"h-80 w-80 bg-slate-600"} />
+            </section>
+            <section className={"flex gap-4"}>
+              <Card className={"h-80 w-80 bg-slate-700"} />
+            </section>
+            <section className={"flex gap-4"}>
+              <Card className={"h-80 w-80 bg-slate-600"} />
+            </section>
+            <section className={"flex gap-4"}>
+              <Card className={"h-80 w-80 bg-slate-700"} />
+            </section>
+            <section className={"flex gap-4"}>
+              <Card className={"h-80 w-80 bg-slate-600"} />
+            </section>
+            <section className={"flex gap-4"}>
+              <Card className={"h-80 w-80 bg-slate-700"} />
+            </section>
+            <section className={"flex gap-4"}>
+              <Card className={"h-80 w-80 bg-slate-600"} />
+            </section>
+            <section className={"flex gap-4"}>
+              <Card className={"h-80 w-80 bg-slate-700"} />
+            </section>
+          </div>
         </div>
       </div>
     </>
