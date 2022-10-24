b642fa59922ce8324d4b7169596df44dd307a88e
kitorer <cockateilcodeh2@gmail.com>
2022-10-24 17:48:07 -0400

    Normal pills

--- mobile/src/components/Button.tsx
+++ mobile/src/components/Button.tsx
@@ -6,8 +6,8 @@ import { Pressable, Text, View } from 'react-native';
 //pass in value as a prop
 export default function ButtonA(props) {
   return (
-    <Pressable className= "rounded-full bg-indigo-500 h-10 w-20 items-center justify-center" >
-        <Text>{props.name}</Text>
+    <Pressable className= "rounded-full bg-indigo-500 h-7 items-center justify-center" >
+        <Text className='text-white margin mx-5'>{props.name}</Text>
     </Pressable>
   );
 }
