e943864561843ab3c6f466124fb44c5347f917e5
kitorer <cockateilcodeh2@gmail.com>
2022-10-24 19:05:57 -0400

    pills and rounded

--- mobile/src/components/Button.tsx
+++ mobile/src/components/Button.tsx
@@ -2,10 +2,18 @@ import { Pressable, Text, View } from 'react-native';
 
 function ButtonPill(props) {
   return (
-    <Pressable className= "rounded-full bg-[#2D1C70] opacity-90 h-7 items-center justify-center" >
+    <Pressable className= "rounded-full bg-[#2D1C70] opacity-95 h-7 items-center justify-center" >
         <Text className='text-white margin mx-5'>{props.name}</Text>
     </Pressable>
   );
 }
 
-export { ButtonPill };
+function ButtonRounded(props) {
+  return (
+    <Pressable className= "rounded-md bg-[#2D1C70] opacity-95 h-7 items-center justify-center" >
+        <Text className='text-white margin mx-5'>{props.name}</Text>
+    </Pressable>
+  );
+}
+
+export { ButtonPill, ButtonRounded };
