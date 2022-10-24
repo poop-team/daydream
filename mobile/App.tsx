23b9a6a3b16c25a9809a3059b26ac039e8c6163a
kitorer <cockatielcodeh2@gmail.com>
2022-10-24 11:10:28 -0400

    tailwinds switch

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,21 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import  BottomNavbar  from './src/components/navbar';
+import { Text, View } from 'react-native';
+
+
 export default function App() {
   const value: string = "hello, world";
   return (
-    <View style={styles.container}>
-      <Text>{value}</Text>
-      <BottomNavbar />
+    <View className="flex-1 justify-center items-center">
+      
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
