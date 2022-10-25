f4d29627ba4dc872ade45d9c3a68f162b19c3143
Aaronel <aaronelharo@hotmail.com>
2022-10-25 16:58:40 -0400

    testing

---
+++ web/src/pages/api/createCollection.ts
@@ -0,0 +1,35 @@
+import { PrismaClient } from "@prisma/client";
+import { NextApiRequest, NextApiResponse } from "next";
+
+import { getServerAuthSession } from "../../server/common/get-server-auth-session";
+
+export default async function createCollection(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const session = await getServerAuthSession({ req, res });
+
+  if (!session) {
+    res.statusCode = 401;
+    return res.json({ Error: "User not logged in." });
+  }
+
+  const prisma = new PrismaClient();
+  const query = req.query;
+
+  if (!query.name) {
+    res.statusCode = 400;
+    return res.json("Give me a name for the collection");
+  }
+
+  const collection = await prisma.collection.create({
+    data: {
+      userId: String(session.user?.id),
+      name: String(query.name),
+    },
+  });
+
+  return res.json({
+    collectionId: collection.id,
+  });
+}
