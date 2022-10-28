63ccfb4bcf38dd96c1866d4d422a4edc587821c1
Robert Boyd III <git@robertboyd.dev>
2022-10-28 18:40:15 -0400

    Add types and some minor tweaks

--- mobile/src/components/Button.tsx
+++ mobile/src/components/Button.tsx
@@ -1,26 +1,34 @@
-import { Pressable, Text, View } from "react-native";
+import { ReactNode } from "react";
+import { Pressable, Text } from "react-native";
+
+interface Props {
+  name: string;
+  className: string;
+  children: ReactNode[];
+  disabled?: boolean;
+  pilled?: boolean;
+}
+
 /**
  * buttonName - name of the button.
  * addButtonStyle - add custom style to the button.
  * children - if you want to add your own text with styles.
  * pilled - make the button pilled (optional).
  * disabled - greys out button (optional).
- **/
+ */
 export default function Button({
-  buttonName,
-  addButtonStyle,
+  name,
+  className,
   children,
   disabled = false,
   pilled = false,
-}) {
+}: Props) {
   let baseStyle = "h-7 items-center justify-center";
   let textStyle = "text-white margin mx-5 font-semibold";
 
   if (disabled) {
     baseStyle += "text-slate-50 bg-indigo-900/40 hover:bg-indigo-900/80";
-  }
-  //default style
-  else {
+  } else {
     baseStyle +=
       "text-slate-50 bg-indigo-900 hover:bg-indigo-800 active:opacity-70";
   }
@@ -31,8 +39,8 @@ export default function Button({
   }
 
   return (
-    <Pressable className={`${baseStyle} ${addButtonStyle}`}>
-      {buttonName && <Text className={textStyle}>{buttonName}</Text>}
+    <Pressable className={`${baseStyle} ${className}`}>
+      {name && <Text className={textStyle}>{name}</Text>}
       {children}
     </Pressable>
   );
