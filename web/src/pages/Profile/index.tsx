c8311dbb508042ad1fb9197e1df039a04cd6000e
dark-r00t <79925808+dark-r00t@users.noreply.github.com>
2022-10-27 21:53:33 -0400

    feat: dynamic card amounts, added search bar, added create button

--- web/src/pages/Profile/index.tsx
+++ web/src/pages/Profile/index.tsx
@@ -1,4 +1,9 @@
+import { SetStateAction, useState } from "react";
+import { MdAccountCircle, MdAddCircle } from "react-icons/md";
+
 import Button from "../../components/Inputs/Button";
+import LinkIconButton from "../../components/Inputs/LinkIconButton";
+import SearchBar from "../../components/Inputs/SearchBar";
 import Card from "../../components/Surfaces/Card";
 
 /*
@@ -9,6 +14,22 @@ import Card from "../../components/Surfaces/Card";
  * */
 
 export default function Index() {
+  const [text, setText] = useState("");
+
+  function handleChange(value: string) {
+    setText(value);
+  }
+
+  const user_views = 1;
+  const user_saves = mockData.length;
+  const user_name = "username";
+  const user_icon = "https://avatars.githubusercontent.com/u/79925808?v=4";
+
+  const saves = [];
+  for (let i = 0; i < user_saves; i++) {
+    saves.push(mockData[i]);
+  }
+
   return (
     <>
       {/* TOP BORDER */}
@@ -24,7 +45,7 @@ export default function Index() {
             {/* PROFILE PICTURE DATA */}
             <img
               className="custom-position h-24 w-24 rounded-full object-cover"
-              src="https://avatars.githubusercontent.com/u/79925808?v=4"
+              src={user_icon}
               alt="name"
             />
           </a>
@@ -36,7 +57,7 @@ export default function Index() {
       {/* USERNAME */}
       <div className="container mx-auto">
         <div className="flex flex-wrap items-center justify-center">
-          <div className="text-lg">@username</div>
+          <div className="text-lg">@{user_name}</div>
         </div>
         <div className="p-1"></div>
       </div>
@@ -44,8 +65,8 @@ export default function Index() {
       {/* VIEWS / SAVED */}
       <div className="container mx-auto">
         <div className="flex flex-wrap items-center justify-center gap-2">
-          <div className="text-sm">0 views</div>
-          <div className="text-sm">0 saves</div>
+          <div className="text-sm">{user_views} views</div>
+          <div className="text-sm">{user_saves} saves</div>
         </div>
         <div className="p-2"></div>
       </div>
@@ -70,9 +91,19 @@ export default function Index() {
 
       {/* SEARCH / +CREATE */}
       <div className="container mx-auto">
-        <div className="p-2"></div>
-        <div className="flex flex-wrap items-center justify-center gap-2">
-          <div className="p-2"></div>
+        <div className="flex flex-wrap items-center justify-center gap-4">
+          <SearchBar
+            value={text}
+            onValueChange={handleChange}
+            className={"w-11/12 max-w-xl sm:w-2/3"}
+          />
+          <LinkIconButton
+            href={"/create"}
+            className={"hidden text-base sm:block"}
+          >
+            {" "}
+            <MdAddCircle className={"h-8 w-full"} />
+          </LinkIconButton>
         </div>
         <div className="p-2"></div>
       </div>
@@ -83,54 +114,15 @@ export default function Index() {
       <div className="container mx-auto">
         <div className="px-4">
           <div className="flex flex-wrap items-center justify-center gap-8">
-            <section className={"flex gap-4"}>
-              <Card className={"h-80 w-80 bg-slate-600"} />
-            </section>
-            <section className={"flex gap-4"}>
-              <Card className={"h-80 w-80 bg-slate-700"} />
-            </section>
-            <section className={"flex gap-4"}>
-              <Card className={"h-80 w-80 bg-slate-600"} />
-            </section>
-            <section className={"flex gap-4"}>
-              <Card className={"h-80 w-80 bg-slate-700"} />
-            </section>
-            <section className={"flex gap-4"}>
-              <Card className={"h-80 w-80 bg-slate-600"} />
-            </section>
-            <section className={"flex gap-4"}>
-              <Card className={"h-80 w-80 bg-slate-700"} />
-            </section>
-            <section className={"flex gap-4"}>
-              <Card className={"h-80 w-80 bg-slate-600"} />
-            </section>
-            <section className={"flex gap-4"}>
-              <Card className={"h-80 w-80 bg-slate-700"} />
-            </section>
-            <section className={"flex gap-4"}>
-              <Card className={"h-80 w-80 bg-slate-600"} />
-            </section>
-            <section className={"flex gap-4"}>
-              <Card className={"h-80 w-80 bg-slate-700"} />
-            </section>
-            <section className={"flex gap-4"}>
-              <Card className={"h-80 w-80 bg-slate-600"} />
-            </section>
-            <section className={"flex gap-4"}>
-              <Card className={"h-80 w-80 bg-slate-700"} />
-            </section>
-            <section className={"flex gap-4"}>
-              <Card className={"h-80 w-80 bg-slate-600"} />
-            </section>
-            <section className={"flex gap-4"}>
-              <Card className={"h-80 w-80 bg-slate-700"} />
-            </section>
-            <section className={"flex gap-4"}>
-              <Card className={"h-80 w-80 bg-slate-600"} />
-            </section>
-            <section className={"flex gap-4"}>
-              <Card className={"h-80 w-80 bg-slate-700"} />
-            </section>
+            {saves.map((images, index) => {
+              return (
+                <div key={index}>
+                  <section className={"flex gap-4"}>
+                    <Card className={"h-80 w-80 bg-slate-600"} />
+                  </section>
+                </div>
+              );
+            })}
           </div>
         </div>
       </div>
@@ -138,7 +130,104 @@ export default function Index() {
   );
 }
 
-/*
-lol
-https://avatars.githubusercontent.com/u/79925808?v=4
-*/
+// GROSS
+const mockData = [
+  {
+    id: 1,
+    src: "/test.png",
+    prompt: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    likes: 69,
+    authorName: "John Doe",
+    authorAvatar: "/test.png",
+  },
+  {
+    id: 2,
+    src: "/test.png",
+    prompt: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.".repeat(
+      100
+    ),
+    likes: 420,
+    authorName: "Elon Musk",
+    authorAvatar: "",
+  },
+  {
+    id: 3,
+    src: "/test.png",
+    prompt: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    likes: 3697,
+    authorName: "Very Long Naaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaame",
+    authorAvatar: "/test.png",
+  },
+  {
+    id: 4,
+    src: "/test.png",
+    prompt: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    likes: 69,
+    authorName: "John Doe",
+    authorAvatar: "/test.png",
+  },
+  {
+    id: 5,
+    src: "/test.png",
+    prompt: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    likes: 69,
+    authorName: "John Doe",
+    authorAvatar: "/test.png",
+  },
+  {
+    id: 6,
+    src: "/test.png",
+    prompt: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    likes: 69,
+    authorName: "John Doe",
+    authorAvatar: "/test.png",
+  },
+  {
+    id: 7,
+    src: "/test.png",
+    prompt: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    likes: 69,
+    authorName: "John Doe",
+    authorAvatar: "/test.png",
+  },
+  {
+    id: 8,
+    src: "/test.png",
+    prompt: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    likes: 69,
+    authorName: "John Doe",
+    authorAvatar: "/test.png",
+  },
+  {
+    id: 9,
+    src: "/test.png",
+    prompt: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    likes: 69,
+    authorName: "John Doe",
+    authorAvatar: "/test.png",
+  },
+  {
+    id: 10,
+    src: "/test.png",
+    prompt: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    likes: 69,
+    authorName: "John Doe",
+    authorAvatar: "/test.png",
+  },
+  {
+    id: 11,
+    src: "/test.png",
+    prompt: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    likes: 69,
+    authorName: "John Doe",
+    authorAvatar: "/test.png",
+  },
+  {
+    id: 12,
+    src: "/test.png",
+    prompt: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    likes: 69,
+    authorName: "John Doe",
+    authorAvatar: "/test.png",
+  },
+];
