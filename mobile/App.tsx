4b378c85f8198d6b19b2a35dd81e77a86a39b989
Daniela Gomez-Dugan <90017168+danielagd@users.noreply.github.com>
2022-10-29 17:24:20 -0400

    Initial Commit of Login on Mobile

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,12 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { Text, View } from 'react-native';
 
 export default function App() {
   const value: string = "hello, world";
   return (
     <View className="flex-1 justify-center items-center">
-      <Text>{value}</Text>
-      <StatusBar style="auto" />
+      
     </View>
   );
 }
