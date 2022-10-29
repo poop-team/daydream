87c7591c80c1a7c6d3e0ed4bff4e9d2c869e614c
Sleepingpirates <faizsyed.a@gmail.com>
2022-10-29 12:57:14 -0400

    trying to comply hardcore prettier rules

--- web/src/pages/api/Allposts.tsx
+++ web/src/pages/api/Allposts.tsx
@@ -5,25 +5,23 @@ export default async function Allposts(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  
-  //returns every single posts can be used for testing on explore page
-  //If userId provided will return all posts from that user.
+//returns every single posts can be used for testing on explore page
+//If userId provided will return all posts from that user.
 
-   const prisma = new PrismaClient();
-   const query = req.query;
-   if(query.userID)
-   {
+  const prisma = new PrismaClient();
+  const query = req.query;
+  if (query.userID)
+  {
     const AllPostsOfUsers = await prisma.post.findMany({
       where: {
           authorId: String(query.userID),
-      }
+    }
     });
     //returns type AllPostsOfAllUsers: Post[]
-    res.json({AllPostsOfUsers});
-   }
-   else{
+    res.json({ AllPostsOfUsers });
+  } else{
     const AllPostsOfAllUsers = await prisma.post.findMany();
     //returns type AllPostsOfAllUsers: Post[]
-    res.json({AllPostsOfAllUsers});
+    res.json({ AllPostsOfAllUsers });
    }
 }
