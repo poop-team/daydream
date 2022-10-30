c880c7517aec33c88fadc87547ababa96f2062d2
Daniela Gomez-Dugan <90017168+danielagd@users.noreply.github.com>
2022-10-30 12:38:09 -0400

    Fixing requested changes

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,11 +1,10 @@
-import React from 'react';
 import { Text, View } from 'react-native';
 
 export default function App() {
   const value: string = "hello, world";
   return (
     <View className="flex-1 justify-center items-center">
-      
+      <Text>{value}</Text>
     </View>
   );
 }
