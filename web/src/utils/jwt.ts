f63a3561348ea29df3a80e8ae6b2f29d716e74a6
Robert Boyd III <git@robertboyd.dev>
2022-10-31 13:59:33 -0400

    feat: add validateRequest() to validate jwt

--- web/src/utils/jwt.ts
+++ web/src/utils/jwt.ts
@@ -1,4 +1,5 @@
-import { sign } from "jsonwebtoken";
+import { sign, verify } from "jsonwebtoken";
+import { NextApiRequest } from "next";
 
 export const generateJWT = async (username: string): Promise<string> => {
   // FIXME we should probably expire more often than 1/day
@@ -22,5 +23,27 @@ export const generateJWT = async (username: string): Promise<string> => {
         }
       }
     );
+  });
+};
+
+export const validateRequest = async (
+  req: NextApiRequest
+): Promise<boolean> => {
+  // eslint-disable-next-line
+  const userId = (req.query.userId as string) ?? req.body.userId;
+  // if you mistakenly pass in null/undefined somehow, don't validate
+  if (!userId) {
+    return Promise.resolve(false);
+  }
+  const authHeader = req.headers.authorization;
+  const matches = authHeader?.match(/Bearer: (.*)/);
+  const jwt = matches?.[1];
+  return new Promise((resolve) => {
+    if (!jwt || !process.env.JWT_PRIVATE_KEY) {
+      throw Error("Missing environment variable process.env.JWT_PRIVATE_KEY");
+    }
+    verify(jwt, process.env.JWT_PRIVATE_KEY, (err, data) => {
+      resolve(!err && data === userId);
+    });
   });
 };
