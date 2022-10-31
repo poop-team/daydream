b321f3a36334a3970b9598da6b66695e250ead10
kitorer <cockatielcodeh2@gmail.com>
2022-10-31 15:42:18 -0400

    prettier, removed & removed unnecessary imports

--- mobile/src/components/Card.tsx
+++ mobile/src/components/Card.tsx
@@ -1,19 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Image, View } from 'react-native';
-interface Props  {
-  url?: string,
-} 
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Image, View } from "react-native";
+interface Props {
+  url?: string;
+}
 
 export default function Card({
-  url = 'https://project.up.railway.app/_next/image?url=https%3A%2F%2Fsbleaping.s3.us-east-1.amazonaws.com%2Fsb%2F9d532691aa47444996dba0e889b6a728.png&w=1080&q=90'
-}:Props) {
-    let description = "This is a placeholder description";
-    return (
-      <Image source={{uri: url}}
-       className ="rounded-lg w-11/12 h-96 mt-5 mx-auto bottom-10"/>
-
-    );
-  }
-
-  
+  url = "https://project.up.railway.app/_next/image?url=https%3A%2F%2Fsbleaping.s3.us-east-1.amazonaws.com%2Fsb%2F9d532691aa47444996dba0e889b6a728.png&w=1080&q=90",
+}: Props) {
+  let description = "This is a placeholder description";
+  return (
+    <Image
+      source={{ uri: url }}
+      className="rounded-lg w-11/12 h-96 mt-5 mx-auto bottom-10"
+    />
+  );
+}
--- mobile/src/pages/feedPage.tsx
+++ mobile/src/pages/feedPage.tsx
@@ -1,30 +1,33 @@
-import { StyleSheet, Text, View, Image, ScrollView, ScrollViewComponent } from 'react-native';
-import BottomNavBar from '../components/BottomNavBar';
-import Card from '../components/Card';
-import TopNavBar from '../components/TopNavBar';
+import {
+  Text,
+  View,
+  ScrollView,
+} from "react-native";
+import BottomNavBar from "../components/BottomNavBar";
+import Card from "../components/Card";
+import TopNavBar from "../components/TopNavBar";
 
 export default function FeedPage() {
-    return (
-      <View className="flex-1 items-center justify-center scroll">
-        <TopNavBar value = "" onChangeText={()=>{}}/>
-        <View className='position-relative w-screen h-screen items-center justify-center top-10'>
-          <ScrollView className="w-screen h-screen position-relative scroll ">
-            <Card url ="https://project.up.railway.app/_next/image?url=https%3A%2F%2Fsbleaping.s3.us-east-1.amazonaws.com%2Fsb%2F7b57ff9681d844198ddca4cbe9b92554.png&w=1080&q=90"/>
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-          </ScrollView>
-        </View >
-        <BottomNavBar />
+  return (
+    <View className="flex-1 items-center justify-center scroll">
+      <TopNavBar value="" onChangeText={() => {}} />
+      <View className="position-relative w-screen h-screen items-center justify-center top-10">
+        <ScrollView className="w-screen h-screen position-relative scroll ">
+          <Card url="https://project.up.railway.app/_next/image?url=https%3A%2F%2Fsbleaping.s3.us-east-1.amazonaws.com%2Fsb%2F7b57ff9681d844198ddca4cbe9b92554.png&w=1080&q=90" />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+        </ScrollView>
       </View>
-    );
-  }
-  
+      <BottomNavBar />
+    </View>
+  );
+}
