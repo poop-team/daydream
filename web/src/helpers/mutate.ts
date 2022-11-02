1e71c6096d0776fb62a1f861b01afe6a450b07db
Rikimbili <raciel2901@gmail.com>
2022-11-02 09:36:03 -0400

    feat(mutate): Add createPost function

--- web/src/helpers/mutate.ts
+++ web/src/helpers/mutate.ts
@@ -1,5 +1,11 @@
 /*
  * Functions to mutate/change data on the API
  */
+import { CreatedPost } from "../types/post.type";
+
+export async function createPost(prompt: string) {
+  const res = await fetch(`/api/createPost?prompt=${prompt}`);
+  return (await res.json()) as CreatedPost;
+}
 export async function likePost(postId: string) {}
 export async function unlikePost(postId: string) {}
