41eba043754c83ead4ee9acc670c4160f5172fa1
Rikimbili <raciel2901@gmail.com>
2022-11-05 15:56:38 -0400

    refactor(request): Catch and throw meaningful error

--- web/src/helpers/request.ts
+++ web/src/helpers/request.ts
@@ -3,6 +3,7 @@
  *
  * @param url - The URL to call (e.g. /api/user/login)
  * @param body - The body of the request
+ * @param method - The HTTP method to use (e.g. POST)
  *
  * @returns The response from the API
  */
@@ -10,20 +11,24 @@ import { getAuthSession } from "../utils/storage";
 
 export default async function doRequest<R extends { error?: string | null }>(
   url: string,
-  body: any
+  body: any,
+  method: "GET" | "POST" | "PUT" | "DELETE" = "GET"
 ) {
   const res = await fetch(url, {
-    method: "POST",
+    method: method,
     headers: {
       "Content-Type": "application/json",
       authorization: `Bearer ${getAuthSession().jwt || "NONE"}`,
     },
     body: JSON.stringify(body),
   });
-  const data = (await res.json()) as R;
+
+  const data = (await res.json().catch(() => {
+    throw new Error("Invalid JSON response");
+  })) as R;
 
   if (!res.ok) {
-    throw new Error(data.error || "Error response received");
+    throw new Error(data?.error || "Error response received");
   }
 
   return data;
