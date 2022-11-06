d62ed90d23066be1bdc0a400c3c9de6a2b65ea9e
Robert Boyd III <git@robertboyd.dev>
2022-11-06 01:03:02 -0400

    fix: use POST for POST routes

--- web/src/helpers/mutate.ts
+++ web/src/helpers/mutate.ts
@@ -7,27 +7,39 @@ import { getAuthSession } from "../utils/storage";
 import doRequest from "./request";
 
 export async function login(email: string, password: string) {
-  return await doRequest<AuthSession>("/api/user/login", {
-    userId: getAuthSession().userId,
-    email,
-    password,
-  });
+  return await doRequest<AuthSession>(
+    "/api/user/login",
+    {
+      userId: getAuthSession().userId,
+      email,
+      password,
+    },
+    "POST"
+  );
 }
 
 export async function register(name: string, email: string, password: string) {
-  await doRequest("/api/user/register", {
-    userId: getAuthSession().userId,
-    name,
-    email,
-    password,
-  });
+  await doRequest(
+    "/api/user/register",
+    {
+      userId: getAuthSession().userId,
+      name,
+      email,
+      password,
+    },
+    "POST"
+  );
 }
 
 export async function createPost(prompt: string) {
-  return await doRequest<CreatedPost>("/api/post/create", {
-    userId: getAuthSession().userId,
-    prompt,
-  });
+  return await doRequest<CreatedPost>(
+    "/api/post/create",
+    {
+      userId: getAuthSession().userId,
+      prompt,
+    },
+    "POST"
+  );
 }
 
 export async function likePost(postId: string) {}
