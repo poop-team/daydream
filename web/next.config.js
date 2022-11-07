ab32220c589d925b2b09f870337a473fe00cfa41
Faiz <32914211+Sleepingpirates@users.noreply.github.com>
2022-11-06 22:07:51 -0500

    Fix: added cloudfront link

--- web/next.config.js
+++ web/next.config.js
@@ -2,6 +2,6 @@ module.exports = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ["sbleaping.s3.us-east-1.amazonaws.com", "oaidalleapiprodscus.blob.core.windows.net"],
+    domains: ["sbleaping.s3.us-east-1.amazonaws.com", "d3603tpinn5q5o.cloudfront.net"],
   }
 };
