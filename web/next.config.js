554421d2e1b41f14ec8545ea26c4368879d02510
Faiz <32914211+Sleepingpirates@users.noreply.github.com>
2022-11-07 10:00:53 -0500

    fix: (config): added CDN link

--- web/next.config.js
+++ web/next.config.js
@@ -2,6 +2,6 @@ module.exports = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ["sbleaping.s3.us-east-1.amazonaws.com", "oaidalleapiprodscus.blob.core.windows.net"],
+    domains: ["sbleaping.s3.us-east-1.amazonaws.com", "d1o142gmp3aap9.cloudfront.net"],
   }
 };
