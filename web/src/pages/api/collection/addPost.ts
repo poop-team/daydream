6042f1c25ae1fa27b75a9636087b5f2157ad6d52
Robert Boyd III <git@robertboyd.dev>
2022-11-07 17:32:21 -0500

    fix: fix formatting to make eslint happy

--- web/src/pages/api/collection/addPost.ts
+++ web/src/pages/api/collection/addPost.ts
@@ -4,49 +4,49 @@ import { prisma } from "../../../server/db/client";
 import { validateRequest } from "../../../utils/jwt";
 
 interface Request extends NextApiRequest {
-    body: {
-        userId: string;
-        postId: string;
-        collectionId: string;
-    };
+  body: {
+    userId: string;
+    postId: string;
+    collectionId: string;
+  };
 }
 
 export default async function addPost(req: Request, res: NextApiResponse) {
-    // Validate if the user has a valid JWT token
-    if (!(await validateRequest(req))) {
-        return res.status(401).json({ error: "User not logged in." });
-    }
+  // Validate if the user has a valid JWT token
+  if (!(await validateRequest(req))) {
+    return res.status(401).json({ error: "User not logged in." });
+  }
 
-    const { userId, postId, collectionId } = req.body;
+  const { userId, postId, collectionId } = req.body;
 
-    if (!postId) {
-        return res.status(400).json("No postId specified in request body");
-    }
+  if (!postId) {
+    return res.status(400).json("No postId specified in request body");
+  }
 
-    // find the post
-    const postToAdd = await prisma.post.findFirst({
-        where: {
-            id: postId,
-        },
-    });
+  // find the post
+  const postToAdd = await prisma.post.findFirst({
+    where: {
+      id: postId,
+    },
+  });
 
-    if (!postToAdd) {
-        return res.status(400).json("Invalid postId");
-    }
+  if (!postToAdd) {
+    return res.status(400).json("Invalid postId");
+  }
 
-    const resStatus = await prisma.collection.update({
-        where: {
-            collectionAndUserId: {
-                userId: userId.toString(),
-                id: collectionId.toString(),
-            },
-        },
-        data: {
-            posts: {
-                connect: { id: postToAdd.id },
-            },
-        },
-    });
+  const resStatus = await prisma.collection.update({
+    where: {
+      collectionAndUserId: {
+        userId: userId.toString(),
+        id: collectionId.toString(),
+      },
+    },
+    data: {
+      posts: {
+        connect: { id: postToAdd.id },
+      },
+    },
+  });
 
-    res.json({ success: true });
+  res.json({ success: true });
 }
--- web/src/pages/api/collection/removePost.ts
+++ web/src/pages/api/collection/removePost.ts
@@ -4,52 +4,52 @@ import { prisma } from "../../../server/db/client";
 import { validateRequest } from "../../../utils/jwt";
 
 interface Request extends NextApiRequest {
-    body: {
-        userId: string;
-        postId: string;
-        collectionId: string;
-    };
+  body: {
+    userId: string;
+    postId: string;
+    collectionId: string;
+  };
 }
 
 // TODO: We should check somewhere that the collection belongs to the user. We don't want to allow users to remove posts from other users' collections.
 export default async function addPostToCollection(
-    req: Request,
-    res: NextApiResponse
+  req: Request,
+  res: NextApiResponse
 ) {
-    // Validate if the user has a valid JWT token
-    if (!(await validateRequest(req))) {
-        return res.status(401).json({ error: "User not logged in." });
-    }
+  // Validate if the user has a valid JWT token
+  if (!(await validateRequest(req))) {
+    return res.status(401).json({ error: "User not logged in." });
+  }
 
-    const { userId, postId, collectionId } = req.body;
+  const { userId, postId, collectionId } = req.body;
 
-    if (!postId) {
-        return res.status(400).json("Give me a post for the collection");
-    }
+  if (!postId) {
+    return res.status(400).json("Give me a post for the collection");
+  }
 
-    const postToAdd = await prisma.post.findFirst({
-        where: {
-            id: String(postId),
-        },
-    });
+  const postToAdd = await prisma.post.findFirst({
+    where: {
+      id: String(postId),
+    },
+  });
 
-    if (!postToAdd) {
-        return res.status(400).json("Invalid PostID");
-    }
+  if (!postToAdd) {
+    return res.status(400).json("Invalid PostID");
+  }
 
-    const resStatus = await prisma.collection.update({
-        where: {
-            collectionAndUserId: {
-                userId: userId.toString(),
-                id: collectionId.toString(),
-            },
-        },
-        data: {
-            posts: {
-                disconnect: { id: postToAdd.id },
-            },
-        },
-    });
+  const resStatus = await prisma.collection.update({
+    where: {
+      collectionAndUserId: {
+        userId: userId.toString(),
+        id: collectionId.toString(),
+      },
+    },
+    data: {
+      posts: {
+        disconnect: { id: postToAdd.id },
+      },
+    },
+  });
 
-    res.status(200).json({ success: true });
+  res.status(200).json({ success: true });
 }
