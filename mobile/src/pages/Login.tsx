0323f775d79cfcc0b8d844b7578e4be9e60e25d4
Robert Boyd III <git@robertboyd.dev>
2022-11-08 12:06:29 -0500

    Prettify code

--- mobile/src/pages/Login.tsx
+++ mobile/src/pages/Login.tsx
@@ -21,66 +21,65 @@ export default function Login({ navigation }) {
           />
         </Pressable>
       </View>
-      <ScrollView className="w-screen h-screen position-relative scroll ">
-
-      <View className="flex-1 flex mx-auto">
-        <Text className=" mt-8 font-bold mb-3 text-xl">
-          Enter your username:
-        </Text>
-
-        <View className=" mx-auto rounded-lg bg-slate-300 w-80 h-12 mb-5 items-start justify-center">
-          <TextInput
-            className="ml-3 w-80"
-            placeholder="*Enter Username"
-            placeholderTextColor="#000000"
-            onChangeText={setUsername}
-          />
-        </View>
-
-        <Text className=" font-bold mb-3 text-xl">Enter a password:</Text>
-
-        <View className="mx-auto rounded-lg bg-slate-300 w-80 mb-5 h-12 items-start justify-center">
-          <TextInput
-            className="ml-3 w-80"
-            secureTextEntry={true}
-            placeholder="*Enter Password"
-            placeholderTextColor="#000000"
-            onChangeText={setPassword}
-          />
-        </View>
-        <Pressable className="items-center justify-center">
-          <Text className="text-xl my-10 text-indigo-900 font-bold">
-            Forgot your password?
+      <ScrollView className="w-screen h-screen position-relative scroll">
+        <View className="flex-1 flex mx-auto">
+          <Text className=" mt-8 font-bold mb-3 text-xl">
+            Enter your username:
           </Text>
-        </Pressable>
-      </View>
-      <View className="flex-1 w-full items-center position-relative justify-center">
-        <Button
-          name="Login"
-          className="mb-10"
-          onPress={() => {
-            login(username, password)
-              .then(async (data) => {
-                if (data) {
-                  storeAuthSession(data);
-                  await navigation.navigate("FeedPage");
-                }
-              })
-              .catch((err: Error) => {
-                //toast.error(err.message);
-              });
-          }}
-        />
-        <Pressable>
-          <Text
-            className="ml-0 text-xl text-indigo-900 font-bold"
-            onPress={() => navigation.navigate("Register")}
-          >
-            Create Account
-          </Text>
-        </Pressable>
-      </View>
-      <StatusBar style="auto" />
+
+          <View className="mx-auto rounded-lg bg-slate-300 w-80 h-12 mb-5 items-start justify-center">
+            <TextInput
+              className="ml-3 w-80"
+              placeholder="*Enter Username"
+              placeholderTextColor="#000000"
+              onChangeText={setUsername}
+            />
+          </View>
+
+          <Text className="font-bold mb-3 text-xl">Enter a password:</Text>
+
+          <View className="mx-auto rounded-lg bg-slate-300 w-80 mb-5 h-12 items-start justify-center">
+            <TextInput
+              className="ml-3 w-80"
+              secureTextEntry={true}
+              placeholder="*Enter Password"
+              placeholderTextColor="#000000"
+              onChangeText={setPassword}
+            />
+          </View>
+          <Pressable className="items-center justify-center">
+            <Text className="text-xl my-10 text-indigo-900 font-bold">
+              Forgot your password?
+            </Text>
+          </Pressable>
+        </View>
+        <View className="flex-1 w-full items-center position-relative justify-center">
+          <Button
+            name="Login"
+            className="mb-10"
+            onPress={() => {
+              login(username, password)
+                .then(async (data) => {
+                  if (data) {
+                    storeAuthSession(data);
+                    await navigation.navigate("FeedPage");
+                  }
+                })
+                .catch((err: Error) => {
+                  //toast.error(err.message);
+                });
+            }}
+          />
+          <Pressable>
+            <Text
+              className="ml-0 text-xl text-indigo-900 font-bold"
+              onPress={() => navigation.navigate("Register")}
+            >
+              Create Account
+            </Text>
+          </Pressable>
+        </View>
+        <StatusBar style="auto" />
       </ScrollView>
     </View>
   );
