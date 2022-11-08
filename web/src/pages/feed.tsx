64d58d5e24e65bf27696d6229f1ab4fae65b052c
Rikimbili <raciel2901@gmail.com>
2022-11-07 21:05:05 -0500

    feat(feed): Infinite scroll + better transitions

--- web/src/pages/feed.tsx
+++ web/src/pages/feed.tsx
@@ -1,30 +1,61 @@
-import { useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery } from "@tanstack/react-query";
+import { useScroll } from "framer-motion";
+import { useEffect, useMemo } from "react";
 import { toast } from "react-hot-toast";
 
 import ImageList from "../components/Layout/ImageList";
-import { fetchPosts } from "../helpers/fetch";
+import { searchPosts } from "../helpers/fetch";
 
-export default function Feed() {
+interface Props {
+  searchValue: string;
+}
+
+export default function Feed({ searchValue }: Props) {
   //#region Hooks
 
-  const { data: posts, isLoading: arePostsLoading } = useQuery(
-    ["feed_posts"],
-    fetchPosts,
-    {
-      onError: (err: Error) => {
-        toast.error(err.message);
-      },
-    }
-  );
+  const { scrollYProgress } = useScroll();
+
+  const {
+    data: infinitePostsData,
+    fetchNextPage,
+    hasNextPage,
+    isFetching,
+    isFetchingNextPage,
+  } = useInfiniteQuery({
+    queryKey: ["feed_posts", { search: searchValue }],
+    queryFn: ({ pageParam = "" }) =>
+      searchPosts({
+        search: searchValue,
+        limit: 32,
+        cursorId: pageParam as string,
+      }),
+    getNextPageParam: (lastPage) => lastPage.nextCursorId,
+    onError: (err: Error) => {
+      toast.error(err.message);
+    },
+  });
+
+  const posts = useMemo(() => {
+    return infinitePostsData?.pages.map((page) => page.posts).flat() ?? [];
+  }, [infinitePostsData]);
+
+  useEffect(() => {
+    return scrollYProgress.onChange((progress) => {
+      if (progress > 0.6 && !isFetchingNextPage && hasNextPage) {
+        void fetchNextPage();
+      }
+    });
+  }, [hasNextPage, isFetchingNextPage]);
 
   //#endregion
 
   return (
     <main className={"h-screen"}>
       <ImageList
-        arePostsLoading={arePostsLoading}
+        arePostsLoading={isFetching && !isFetchingNextPage}
+        areMorePostsLoading={isFetchingNextPage}
         posts={posts}
-        className={"px-2 pb-4 pt-16 sm:px-4 lg:px-8"}
+        className={"px-2 py-16 sm:px-4 md:pb-8 lg:px-8"}
       />
     </main>
   );
