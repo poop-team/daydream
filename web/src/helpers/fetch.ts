249c7fea2e5e6b32254290f238c58a99d2dcb413
Rikimbili <raciel2901@gmail.com>
2022-11-09 17:50:53 -0500

    feat(fetch): Add getUser and authenticateUser

--- web/src/helpers/fetch.ts
+++ web/src/helpers/fetch.ts
@@ -3,29 +3,40 @@
  */
 
 import { Post } from "../types/post.type";
+import { User } from "../types/user.type";
 import { getAuthSession } from "../utils/storage";
 import doRequest from "./request";
-
-interface Params {
-  search?: string;
-  userId?: string | "me";
-  collectionId?: string;
-  limit?: number | string;
-  cursorId?: string;
-}
 
 export async function searchPosts({
   search = "",
   userId = "",
   collectionId = "",
-  limit = "",
+  limit = 16,
   cursorId = "", // cursorId
-}: Params) {
-  const myUserId = getAuthSession().userId;
+}) {
   return await doRequest<{ posts: Post[]; nextCursorId: string }>(
-    `/api/post/search?userId=${myUserId}&search=${search}&searchUserId=${
-      userId === "me" ? myUserId : userId
-    }&collectionId=${collectionId}&limit=${limit}&cursorId=${cursorId}`,
+    `/api/post/search?userId=${
+      getAuthSession().userId
+    }&search=${search}&searchUserId=${userId}&collectionId=${collectionId}&limit=${limit}&cursorId=${cursorId}`,
+    null,
+    "GET"
+  );
+}
+
+export async function getUser(userId = "") {
+  const currentUserId = getAuthSession().userId;
+  return await doRequest<User>(
+    `/api/user/get?userId=${currentUserId}&searchUserId=${
+      userId || currentUserId
+    }`,
+    null,
+    "GET"
+  );
+}
+
+export async function authenticateUser() {
+  return await doRequest<{ message: string }>(
+    `/api/user/auth?userId=${getAuthSession().userId}`,
     null,
     "GET"
   );
