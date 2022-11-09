02a67028fe0e296c5aee4062c7109352556330b5
Rikimbili <raciel2901@gmail.com>
2022-11-09 17:53:29 -0500

    refactor(api/user/get): Use searchUserId param

--- web/src/pages/api/user/get.ts
+++ web/src/pages/api/user/get.ts
@@ -3,35 +3,43 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { prisma } from "../../../server/db/client";
 import { validateRequest } from "../../../utils/jwt";
 
-interface Request extends NextApiRequest {
-  body: {
-    userId: string;
-  };
-}
-
-export default async function get(req: Request, res: NextApiResponse) {
+export default async function get(req: NextApiRequest, res: NextApiResponse) {
   // Validate if the user has a valid JWT token
   if (!(await validateRequest(req))) {
     return res.status(401).json({ error: "User not logged in." });
   }
 
-  const { userId } = req.body;
+  const { searchUserId } = req.query;
 
-  if (!userId) {
+  if (!searchUserId) {
     return res
       .status(400)
       .json({ error: "You are missing the userId parameter" });
   }
 
-  if (Array.isArray(userId)) {
+  if (Array.isArray(searchUserId)) {
     return res.status(400).json({ error: "userId cannot be a string array" });
   }
 
-  const user = await prisma.user.findUnique({
-    where: {
-      id: userId,
-    },
-  });
-
-  res.json({ user });
+  prisma.user
+    .findUnique({
+      where: {
+        id: searchUserId,
+      },
+      select: {
+        id: true,
+        name: true,
+        image: true,
+      },
+    })
+    .then((user) => {
+      if (!user) {
+        return res.status(404).json({ error: "User not found" });
+      }
+      res.json(user);
+    })
+    .catch((e: Error) => {
+      console.error(e.message);
+      res.status(500).json({ error: "Internal database error" });
+    });
 }
