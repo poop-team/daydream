a66f9f4d14687f7d310891d53a9ab12af0d62a8e
aMarshmallows <amasha667@gmail.com>
2022-11-09 20:13:57 -0500

    create getpost endpoint

---
+++ web/src/pages/api/post/getPost.ts
@@ -0,0 +1,50 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+import { prisma } from "../../../server/db/client";
+import { validateRequest } from "../../../utils/jwt";
+
+interface Request extends NextApiRequest {
+  body: {
+    postId: string;
+  };
+}
+
+export default async function get(req: Request, res: NextApiResponse) {
+  // Validate if the user has a valid JWT token
+  if (!(await validateRequest(req))) {
+    return res.status(401).json({ error: "User not logged in." });
+  }
+
+  const { postId } = req.body;
+
+  if (!postId) {
+    return res
+      .status(400)
+      .json({ error: "You are missing the postId parameter" });
+  }
+
+  if (Array.isArray(postId)) {
+    return res.status(400).json({ error: "postId cannot be a string array" });
+  }
+
+  const post = await prisma.post.findUnique({
+    where: {
+      id: postId,
+    },
+    select: {
+      id: true,
+      dateCreated: true,
+      prompt: true,
+      imageURL: true,
+      author: {
+        select: {
+          name: true,
+          id: true,
+        },
+      },
+      likes: true,
+    },
+  });
+
+  res.json({ post });
+}
