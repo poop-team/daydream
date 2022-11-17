bab6f48b2fd5509fe72c059a45c23cb530f05dac
Sleepingpirates <faizsyed.a@gmail.com>
2022-11-17 17:32:16 -0500

    refactor: switch to replicate

--- web/next.config.js
+++ web/next.config.js
@@ -2,7 +2,7 @@ module.exports = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ["sbleaping.s3.us-east-1.amazonaws.com", "d1o142gmp3aap9.cloudfront.net"],
+    domains: ["sbleaping.s3.us-east-1.amazonaws.com", "replicate.delivery"],
   },
   async redirects() {
     return [
