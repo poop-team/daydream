7ef682d3570fc8d3493748e70dc37fd955c8734d
kitorer <cockatielcodeh2@gmail.com>
2022-11-18 00:08:31 -0500

    +safeAreaView

--- mobile/src/pages/feedPage.tsx
+++ mobile/src/pages/feedPage.tsx
@@ -1,11 +1,13 @@
 import { View, ScrollView } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 import BottomNavBar from "../components/BottomNavBar";
 import Card from "../components/Card";
 import TopNavBar from "../components/TopNavBar";
 
 export default function FeedPage({ navigation }) {
   return (
-    <View className="flex-1 items-center justify-center scroll">
+    <View className="flex-1">
+      <SafeAreaView className="flex-1">
       <TopNavBar value="" onChangeText={() => {}} />
       <View className="position-relative w-screen h-screen items-center justify-center top-10">
         <ScrollView className="w-screen h-screen position-relative scroll ">
@@ -26,6 +28,7 @@ export default function FeedPage({ navigation }) {
           <Card />
         </ScrollView>
       </View>
+      </SafeAreaView>
       <BottomNavBar navigation={navigation} />
     </View>
   );
