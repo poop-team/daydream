546be579d1d98dedccc6910f308d8868b5b35bca
“Sleepingpirates” <faizsyed.a@gmail.com>
2022-11-20 11:06:54 -0500

    refactor: remove console.logs used for testing

--- web/src/pages/api/user/reset.ts
+++ web/src/pages/api/user/reset.ts
@@ -23,7 +23,6 @@ export default async function reset(req: Request, res: NextApiResponse) {
   });
 
   if (!user) {
-    console.log("User not found");
     // If the user does not exist, we don't want to let them know
     // that the email address is not in our system
     // so we just return a success message
@@ -33,7 +32,6 @@ export default async function reset(req: Request, res: NextApiResponse) {
   // Generate a Reset JWT token with a 20 min expiry time.
   //This token will be used to verify the user's identity when they click the reset link in their email
   const token = await generateJWTForPasswordReset(user.id);
-  console.log(token);
   //create url for reset link
   //const resetUrl = `${process.env.NEXT_PUBLIC_APP_URL}/api/user/resetVerify?token=${token}`;
   //send email with reset link to user
