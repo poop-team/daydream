63fbde1a31d510d6ad670ee395f854ec35497e65
kitorer <cockatielcodeh2@gmail.com>
2022-11-21 21:09:51 -0500

    image pagination wip

--- mobile/src/pages/feedPage.tsx
+++ mobile/src/pages/feedPage.tsx
@@ -1,31 +1,26 @@
-import { View, ScrollView } from "react-native";
+import { useState } from "react";
+import { View, ScrollView, Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import BottomNavBar from "../components/BottomNavBar";
 import Card from "../components/Card";
 import TopNavBar from "../components/TopNavBar";
+import { getAuthSession } from "../utils/storage";
+import * as SecureStore from "expo-secure-store";
 
 export default function FeedPage({ navigation }) {
+
+  //region hooks
+  const [search, setSearch] = useState("");
+  
+  //end region
+
   return (
     <View className="flex-1">
       <SafeAreaView className="flex-1">
-      <TopNavBar value="" onChangeText={() => {}} />
+      <TopNavBar value="" onChangeText={setSearch} />
       <View className="position-relative w-screen h-screen items-center justify-center top-10">
         <ScrollView className="w-screen h-screen position-relative scroll ">
-          <Card
-            url="https://project.up.railway.app/_next/image?url=https%3A%2F%2Fsbleaping.s3.us-east-1.amazonaws.com%2Fsb%2F7b57ff9681d844198ddca4cbe9b92554.png&w=1080&q=90"
-            className="h-40"
-          />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
+          
         </ScrollView>
       </View>
       </SafeAreaView>
