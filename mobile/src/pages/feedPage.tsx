8d3a0f310f7291724587d3cbaf3910d9f46caaac
kitorer <cockatielcodeh2@gmail.com>
2022-11-25 02:31:43 -0500

    todo: add pagination; query & display done

--- mobile/src/pages/feedPage.tsx
+++ mobile/src/pages/feedPage.tsx
@@ -1,28 +1,37 @@
-import { useState } from "react";
-import { View, ScrollView, Text } from "react-native";
+import { useState, useMemo } from "react";
+import { View, ScrollView } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import BottomNavBar from "../components/BottomNavBar";
 import Card from "../components/Card";
 import TopNavBar from "../components/TopNavBar";
-import { getAuthSession } from "../utils/storage";
-import * as SecureStore from "expo-secure-store";
+import useAuthGetStorage from "../hooks/useGetAuthStorage";
+import ImageList from "../hooks/useInfiniteQueryPosts";
 
 export default function FeedPage({ navigation }) {
-
   //region hooks
   const [search, setSearch] = useState("");
-  
-  //end region
 
+  //this blob probably needs to be moved to a different file
+  const blob = ImageList({
+    searchValue: search,
+    recentOnly: false,
+    key: "feedPage",
+  });
+  //end region
   return (
     <View className="flex-1">
       <SafeAreaView className="flex-1">
-      <TopNavBar value="" onChangeText={setSearch} />
-      <View className="position-relative w-screen h-screen items-center justify-center top-10">
-        <ScrollView className="w-screen h-screen position-relative scroll ">
-          
-        </ScrollView>
-      </View>
+        <TopNavBar value="" onChangeText={setSearch} />
+        <View className="position-relative w-screen h-screen items-center justify-center top-10">
+          <ScrollView className="w-screen h-screen position-relative scroll ">
+            {
+              //this is where the posts will go
+              blob.posts.map((post) => (
+                <Card url={post.imageURL} />
+              ))
+            }
+          </ScrollView>
+        </View>
       </SafeAreaView>
       <BottomNavBar navigation={navigation} />
     </View>
