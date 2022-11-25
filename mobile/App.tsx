9c547dbd7031ba3d48d0fcbd7ee83b9468508b9b
kitorer <cockatielcodeh2@gmail.com>
2022-11-25 02:04:23 -0500

    added query client to main stack

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -8,45 +8,49 @@ import ImageCreate from "./src/pages/ImageCreate";
 
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const Stack = createStackNavigator();
+const queryClient = new QueryClient();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Home"
-          component={Authentication}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="Login"
-          component={Login}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="Register"
-          component={Register}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="FeedPage"
-          component={FeedPage}
+    <QueryClientProvider client={queryClient}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Home"
+            component={Authentication}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="Login"
+            component={Login}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="Register"
+            component={Register}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="FeedPage"
+            component={FeedPage}
           options={{headerShown: false}}
-        />
-        <Stack.Screen
-          name="createPage"
-          component={ImageCreate}
+          />
+          <Stack.Screen
+            name="createPage"
+            component={ImageCreate}
           options={{headerShown: false}}
-        />
+          />
         <Stack.Screen
           name="profilePage"
           options={{headerShown: false}}
         >
           {(props) => <Dummypage {...props} text= "profile" />}
-        </Stack.Screen>    
-      </Stack.Navigator>
-    </NavigationContainer>
+          </Stack.Screen>
+        </Stack.Navigator>
+      </NavigationContainer>
+    </QueryClientProvider>
   );
 }
