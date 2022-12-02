9bfc105c951e31da4a0df12a20e5f36b8e44c334
Robert Boyd III <git@robertboyd.dev>
2022-12-02 18:36:08 -0500

    Remove hard-coded credentials

--- mobile/src/pages/Login.tsx
+++ mobile/src/pages/Login.tsx
@@ -62,7 +62,7 @@ export default function Login({ navigation }) {
             disabled={isPending || invalidCount >= 5}
             onPress={() => {
               setIsPending(true);
-              login("danigomezdugan@gmail.com", "password")
+              login(email, password)
                 .then(async (data) => {
                   if (data) {
                     storeAuthSession(data);
