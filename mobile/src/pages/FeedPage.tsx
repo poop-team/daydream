954d2ceae415e376093866bff285150675dc1b93
kitorer <cockatielcodeh2@gmail.com>
2022-12-03 11:09:20 -0500

    hook const changes

--- mobile/src/pages/FeedPage.tsx
+++ mobile/src/pages/FeedPage.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { View } from "react-native";
+import { ActivityIndicator, View } from "react-native";
 import { FlatList } from "react-native-gesture-handler";
 import { SafeAreaView } from "react-native-safe-area-context";
 import BottomNavBar from "../components/BottomNavBar";
@@ -12,31 +12,28 @@ export default function FeedPage({ navigation }) {
   const [search, setSearch] = useState("");
 
   const debouncedSearch = useDebounce(search, 500);
-  const blob = useInfiniteQueryPosts({
+  const {posts, isFetching, isFetchingNextPage, fetchNextPage} = useInfiniteQueryPosts({
     searchValue: debouncedSearch,
     limit: 8,
     recentOnly: false,
     key: "feed_posts",
   });
 
-  let pageHeight = 0;
-  let pageProgress = 0;
   return (
     <View className="flex-1">
       <SafeAreaView className="flex-1">
         <TopNavBar onChangeText={setSearch} />
-        <View className="position-relative w-screen items-center justify-center android:mt-3">
-          {/*surely there is a better way to keep the scroll view above the bottomNav*/}
+        <View className="position-relative w-screen items-center justify-evenly android:mt-3">
           <FlatList
             className="w-screen android:mb-12"
-            data={blob.posts}
+            data={posts}
             renderItem={({ item }) => (
               <Card key={item.id} url={item.imageURL} />
             )}
             keyExtractor={(item) => item.id}
             onEndReached={() => {
-              if (!blob.isFetching) {
-                blob.fetchNextPage();
+              if (!isFetching) {
+                fetchNextPage();
               }
             }}
             onEndReachedThreshold={0.5}
