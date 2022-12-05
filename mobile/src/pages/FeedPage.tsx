5ac51770707c0f0be2d982960c01aec49a4c898e
kitorer <cockatielcodeh2@gmail.com>
2022-12-04 22:09:32 -0500

    CardUpdate

--- mobile/src/pages/FeedPage.tsx
+++ mobile/src/pages/FeedPage.tsx
@@ -31,7 +31,7 @@ export default function FeedPage({ navigation }) {
             className="w-screen android:mb-12"
             data={blob.posts}
             renderItem={({ item }) => (
-              <Card key={item.id} url={item.imageURL} />
+              <Card key={item.id} url={item.imageURL} data={item}/>
             )}
             keyExtractor={(item) => item.id}
             onScroll={(event) => {
