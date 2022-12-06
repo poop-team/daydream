1b4cb79b29d4849c7fa7ec0759719257127c861d
dark-r00t <79925808+dark-r00t@users.noreply.github.com>
2022-12-06 00:03:54 -0500

    feat(mobile/collection) add clickable collections

--- mobile/src/components/CollectionCard.tsx
+++ mobile/src/components/CollectionCard.tsx
@@ -1,30 +1,56 @@
-import {Image, Pressable, Text, View} from "react-native";
+import {Image, Modal, Pressable, Text, View} from "react-native";
 import Card from "./Card";
 import Collection from "../types/collection.type";
+import React from "react";
+import Icon from "react-native-vector-icons/Ionicons";
 
 interface Props {
   collection?: Collection;
 }
 
 function CollectionCard({
-                          collection,
-                        }: Props) {
+  collection,
+}: Props) {
+  const [modalVisible, setModalVisible] = React.useState(false);
   return (
-    <View key={collection.id} className="flex aspect-square rounded-lg h-[48vw]">
-      <Pressable onPress={() =>
-        {collection.posts.map((post) => (
-          <Card
-            key={post.id}
-            className="rounded-lg h-[48vw] aspect-square"
-            post={post}
-          />
-        ))}
+    <View>
+      <Modal
+        visible={modalVisible}
+        onRequestClose={() => {
+          setModalVisible(!modalVisible);
+        }
       }>
+        <View className="w-full h-full bg-white">
+          <View className="w-full flex-row place-content-start ml-2">
+            <Pressable>
+              <Icon
+                size={40}
+                name="arrow-back-outline"
+                onPress={() => {
+                  setModalVisible(!modalVisible);
+                }}
+              />
+            </Pressable>
+          </View>
+          <View>
+            {collection.posts.map((post) => (
+              <Card
+                key={post.id}
+                className="rounded-lg h-[48vw] aspect-square"
+                post={post}
+              />
+            ))}
+          </View>
+        </View>
+      </Modal>
+      <View key={collection.id} className="flex aspect-square rounded-lg h-[48vw]">
         {collection.posts[0]?.imageURL ?
-          <Image
-            source={{uri: collection.posts[0]?.imageURL}}
-            className="h-[77%]"
-          /> :
+          <Pressable onPress={() => setModalVisible(true)}>
+            <Image
+              source={{uri: collection.posts[0]?.imageURL}}
+              className="h-[77%]"
+            />
+          </Pressable>:
           <View className="flex flex-1 items-center justify-center bg-slate-300/80">
             <Text className="text-xl font-semibold text-slate-600"> {/*TOOD not displaying box anymore*/}
               Nothing saved yet
@@ -37,7 +63,7 @@ function CollectionCard({
         <Text>
           {collection.posts.length} saved
         </Text>
-      </Pressable>
+      </View>
     </View>
   )
 }
