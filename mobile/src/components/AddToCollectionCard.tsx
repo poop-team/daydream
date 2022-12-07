9c21f1f512eaa6b002db1d57fdbe4b074b5cbf5e
dark-r00t <79925808+dark-r00t@users.noreply.github.com>
2022-12-06 23:36:51 -0500

    feat(mobile/add2): add ability to see collection posts from card view

---
+++ mobile/src/components/AddToCollectionCard.tsx
@@ -0,0 +1,64 @@
+import {
+  Dimensions,
+  Image,
+  Pressable,
+  Text,
+  View,
+} from "react-native";
+import Collection from "../types/collection.type";
+import {styled} from "nativewind";
+import {addPostToCollection} from "../requests/mutate";
+
+interface Props {
+  postId: string;
+  collection?: Collection;
+}
+
+function CollectionCard({postId, collection}: Props) {
+  const screenWidth = Dimensions.get("window").width;
+  return (
+    <View>
+      <View
+        key={collection.id}
+        className="flex aspect-square rounded-lg h-[48vw]"
+      >
+        {collection.posts[0]?.imageURL ? (
+          <>
+            <Pressable
+              onPress={() => {
+                //FIXME: added logic to check if post is already in collection and either add/delete respectively
+                console.log("something1")
+              }}
+            >
+              <Image
+                source={{uri: collection.posts[0]?.imageURL}}
+                style={{minHeight: screenWidth * 0.48 - 48}}
+              />
+              <Text className="font-bold">{collection.name}</Text>
+              <Text>{collection.postCount} saved</Text>
+            </Pressable>
+          </>
+        ) : (
+          <>
+            <View className="flex flex-1 items-center justify-center bg-slate-300/80">
+              <Pressable
+                onPress={() => {
+                  //FIXME: added logic to check if post is already in collection and either add/delete respectively
+                  console.log("something2")
+                }}
+              >
+                <Text className="text-xl font-semibold text-slate-600">
+                  Nothing saved yet
+                </Text>
+              </Pressable>
+            </View>
+            <Text className="font-bold">{collection.name}</Text>
+            <Text>{collection.posts.length} saved</Text>
+          </>
+        )}
+      </View>
+    </View>
+  );
+}
+
+export default styled(CollectionCard);
--- mobile/src/components/Card.tsx
+++ mobile/src/components/Card.tsx
@@ -1,5 +1,5 @@
-import { styled } from "nativewind";
-import React from "react";
+import {styled} from "nativewind";
+import React, {useEffect, useState} from "react";
 import {
   Image,
   ImageStyle,
@@ -13,32 +13,106 @@ import {
 import Button from "./Button";
 import Icon from "react-native-vector-icons/Ionicons";
 import useLikePost from "../hooks/useLikePost";
+import {getAuthSession} from "../utils/storage";
+import {useQuery} from "@tanstack/react-query";
+import {getCollections, getUser} from "../requests/fetch";
+import AddToCollectionCard from "./AddToCollectionCard";
 
 //fixme: I will probably have to break card up unto a sub-component like the web version and then url will be required
 interface Props {
   style?: StyleProp<ImageStyle>;
   post?: any;
 }
+
 /**
  * @url url of the image string (default: muppet (<3) )
  * @styles width & height (default h-96, w-11/12)
  * @data data from the useInfiniteQueryPosts hook
  */
-function Card({ style, post }: Props) {
+
+function Card({style, post}: Props) {
+
   //these spaces are here so that the user of this api cannot mess it up if they forget to add a space
+  const [userId, setUserId] = useState(null);
+  const [selectedPost, setSelectedPost] = useState(null);
+  const [atcCardVisible, showAtCCard] = React.useState(false);
   const [modalVisible, setModalVisible] = React.useState(false);
-  const { mutateLikeChange, isLiked, likeCount } = useLikePost({
+  const {mutateLikeChange, isLiked, likeCount} = useLikePost({
     postId: post?.id,
     initialLikeCount: post?.likeCount,
     initialIsLiked: post?.isLiked,
   });
 
+  {/*FIXME REFACTOR: JAN START*/}
+  useEffect(() => {
+    (async () => {
+      const { userId } = await getAuthSession();
+      setUserId(userId);
+    })();
+  }, []);
+
+  const {
+    data: collectionData
+  } = useQuery({
+    queryKey: ["user_collections", userId],
+    queryFn: () => getCollections({ userId }),
+    onError: (err: Error) => {
+      console.error(err.message);
+    },
+    enabled: !!userId,
+  });
+
+  const collections = collectionData?.collections ?? []
+  {/*FIXME REFACTOR: JAN STOP*/}
+
   let baseStyle = "rounded-lg mt-5 mx-auto";
+
   const url =
     post?.imageURL ||
     "https://project.up.railway.app/_next/image?url=https%3A%2F%2Fsbleaping.s3.us-east-1.amazonaws.com%2Fsb%2F9d532691aa47444996dba0e889b6a728.png&w=1080&q=90";
+
   return (
     <View>
+      {/*FIXME REFACTOR: JAN START*/}
+      <Modal
+        animationType="fade"
+        transparent={true}
+        visible={atcCardVisible}
+        onRequestClose={() => {
+          showAtCCard(!atcCardVisible);
+        }}
+      >
+        <View style={styles.modalView}>
+          <View className="h-[90%] bg-white my-auto rounded-lg">
+            <View className="w-full flex-row ml-2 mt-2 justify-start">
+              <Pressable>
+                <Icon
+                  size={40}
+                  name="arrow-back-outline"
+                  onPress={() => {
+                    showAtCCard(!atcCardVisible);
+                  }}
+                />
+              </Pressable>
+            </View>
+            <View className="h-[92%] bg-white my-auto rounded-lg">
+              <View className="w-full flex flex-row flex-wrap justify-evenly gap-y-1 my-1">
+                {collections.map((collection) => (
+                  <AddToCollectionCard
+                    postId={selectedPost}
+                    key={collection.id}
+                    collection={collection}
+                  />
+                ))}
+                {collections.length % 2 === 1 && (
+                  <View className="rounded-lg h-[48vw] aspect-square" />
+                )}
+              </View>
+            </View>
+          </View>
+        </View>
+      </Modal>
+      {/*FIXME REFACTOR: JAN STOP*/}
       <Modal
         animationType="fade"
         transparent={true}
@@ -60,7 +134,7 @@ function Card({ style, post }: Props) {
                 />
               </Pressable>
             </View>
-            <Image className="w-11/12 h-96 rounded-lg" source={{ uri: url }} />
+            <Image className="w-11/12 h-96 rounded-lg" source={{uri: url}}/>
             <View className="flex w-full flex-col items-center gap-2 p-4 md:p-6 lg:gap-4">
               <View className="flex-row align-middle justify-between">
                 <View className="w-5/6 flex-row c">
@@ -72,17 +146,20 @@ function Card({ style, post }: Props) {
                 {/*FIXME: LIKE COUNTER NEEDS TO BE UPDATEABLE*/}
                 <Pressable onPress={() => mutateLikeChange(!isLiked)}>
                   {isLiked ? (
-                    <Icon name="heart" color="red" size={20} />
+                    <Icon name="heart" color="red" size={20}/>
                   ) : (
-                    <Icon name="heart-outline" size={20} />
+                    <Icon name="heart-outline" size={20}/>
                   )}
                 </Pressable>
               </View>
               <Text>{post?.prompt}</Text>
-              <Button pilled>
+              <Button pilled onPress={() => {
+                showAtCCard(!atcCardVisible)
+                setSelectedPost(post.id)
+              }}>
                 <View className="flex-row">
                   <Text className="text-white">Add to Collection </Text>
-                  <Icon name="albums-outline" size={20} color="white" />
+                  <Icon name="albums-outline" size={20} color="white"/>
                 </View>
               </Button>
             </View>
@@ -91,7 +168,7 @@ function Card({ style, post }: Props) {
       </Modal>
       <Pressable onPress={() => setModalVisible(true)}>
         <Image
-          source={{ uri: url }}
+          source={{uri: url}}
           className={`${baseStyle} h-96 w-11/12`}
           style={style}
         />
